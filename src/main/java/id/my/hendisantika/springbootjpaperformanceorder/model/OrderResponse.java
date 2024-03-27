a3306a48b41058d67424c44d86c0ac320d0c2c4b
hendisantika <hendisantika@yahoo.co.id>
2024-03-27 07:25:55 +0700

    Add OrderResponse 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootjpaperformanceorder/model/OrderResponse.java
@@ -0,0 +1,25 @@
+package id.my.hendisantika.springbootjpaperformanceorder.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jpa-performance-order
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/27/24
+ * Time: 07:25
+ * To change this template use File | Settings | File Templates.
+ */
+public record OrderResponse(Long orderId,
+                            String name,
+                            LocalDateTime orderDate,
+                            List<ItemResponse> items,
+                            int totalPrice,
+                            OrderStatus orderStatus,
+                            AddressResponse address
+) {
+}
+
