5f38d80222d6bfa9dff818a4bfb27ff02f47320b
hendisantika <hendisantika@yahoo.co.id>
2024-03-27 07:20:06 +0700

    Add Member Model 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootjpaperformanceorder/model/Member.java
@@ -0,0 +1,41 @@
+package id.my.hendisantika.springbootjpaperformanceorder.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jpa-performance-order
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/27/24
+ * Time: 07:19
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+}
