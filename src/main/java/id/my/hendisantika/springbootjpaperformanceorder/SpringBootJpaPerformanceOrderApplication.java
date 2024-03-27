a196496bc8c3632aadd8c6727008a12fb6300c22
hendisantika <hendisantika@yahoo.co.id>
2024-03-27 07:14:13 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootjpaperformanceorder/SpringBootJpaPerformanceOrderApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootjpaperformanceorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaPerformanceOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaPerformanceOrderApplication.class, args);
+    }
+
+}
